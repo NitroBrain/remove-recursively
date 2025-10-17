0b345f8326ccad0b3c8fd8eec1d2dbe4875c49a2
Shokhrukhmirzo Usmonov <shokhrukhmirzo_usmonov@student.itpu.uz>
2025-10-17 13:18:16 +0500

    init: add new Console App

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          if (args.Length == 0)
+          {
+            Console.WriteLine("Usage: rr [folder1] [folder2] ...");
+            return;
+          }
+        }
+    }
+}
