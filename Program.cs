98247073bb2111721578f309983569be0e1b66d0
Shokhrukhmirzo Usmonov <shokhrukhmirzo_usmonov@student.itpu.uz>
2025-10-17 13:29:34 +0500

    feat(cli): add implementation for parsing folder and arguments

--- Program.cs
+++ Program.cs
@@ -9,6 +9,15 @@
             Console.WriteLine("Usage: rr [folder1] [folder2] ...");
             return;
           }
+
+          string currentPath = Directory.GetCurrentDirectory();
+          var folders = new List<string>(args);
+
+          Console.WriteLine($"Starting from: {currentPath}");
+          foreach (var folder in folders)
+          {
+            Console.WriteLine($"Looking for folder: {folder}");
+          }
         }
     }
 }
